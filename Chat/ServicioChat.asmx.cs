b4cdc742796abe8d934817855e18de65909519d1
iliana <ili_takajasi@hotmail.com>
2015-04-29 14:37:05 -0500

    Mi primer chat

---
+++ Chat/ServicioChat.asmx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Chat
+{
+    /// <summary>
+    /// Descripción breve de ServicioChat
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicioChat : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string chatiar(string Mensaje)
+        {
+            switch (Mensaje)
+            {
+                case "Hola bb":  return"Hola mucho gusto";
+                case "De donde eres": return "de Estados Unidos";
+                case "Como estas": return "Muy buena";
+                case "Como te llamas": return "Kimberly";
+                case "Tienes cel": return "Si pero no se lo doy a cualquiera";
+                case "Tienes web cam": return "No tengo";
+                case "Quieres mostrarme": return "Que te muestre que ??";
+                case "Que buscas realmente": return "Busco dinero y no es interes";
+                case "Osea ke te gusta experimentar cosas nuevas": return "No jamas haga experimentos";
+                case "Que tal si empezamos a subir la temperatura": return "No gracias ";
+                case "Hasta donde quieres llegar": return "AQuiero llegar algun dia a Japon";
+                case "Cuantos años tienes": return "15";
+                case "Que te gusta hacer": return "No mucho";
+                case "Y lo...": return "LUEGO QUE??";
+                case "Ahorita llego a tu casa": return "Por que me estas acosando";
+                default: return "Favor de Contestar";
+                    
+            }
+        }
+    }
+}
---
+++ SaladeChat/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SaladeChat.SRChat;
+
+namespace SaladeChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            SRChat.ServicioChatSoapClient Ili = new ServicioChatSoapClient();
+            var respuesta=Ili.chatiar(txtMensaje.Text);
+            lstSala.Items.Add(txtMensaje.Text);
+            lstSala.Items.Add(respuesta);
+
+        }
+    }
+}
